4cb5f39d06bb4e8e938564d2f5947d0fd7bb05b7
Margo Art <margoart.work@gmail.com>
2017-11-10 20:41:13 -0600

    deleted boilerplate files

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import BusinessList from '../BusinessList/BusinessList';
-import SearchBar from '../SearchBar/SearchBar';
+import BusinessList from './BusinessList/BusinessList';
+import SearchBar from './SearchBar/SearchBar';
 
-class App extends Component {
+class App extends React.Component {
     render() {
         return (
             <div className="App">
