0d4f7584ee3297bdf7a3eac0af19ef6d805c4d78
Margarita Artiushenko <32603867+yummymell0w@users.noreply.github.com>
2020-02-19 23:17:57 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,30 +4,6 @@ import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
 import Yelp from './util/Yelp';
 
-/* removed the following info as per step 35 of Ravenous lV
-const business = {
-    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90
-};
-
-
-const businesses = [
-    business,
-    business,
-    business,
-    business,
-    business,
-    business
-];
-*/
-
 class App extends React.Component {
     constructor(props) {
         super(props);
